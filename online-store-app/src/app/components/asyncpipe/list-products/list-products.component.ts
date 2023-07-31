c80275fae67392831ec360ceec2c18f9ff540690
Teklit Berihu Gereziher <teklit.berihu.gereziher@diadrom.se>
2023-07-31 16:48:44 +0200

    Use async pipe instead of manual change detection

--- online-store-app/src/app/components/asyncpipe/list-products/list-products.component.ts
+++ online-store-app/src/app/components/asyncpipe/list-products/list-products.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { IProduct } from '../models/product';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list-products',
@@ -9,30 +9,31 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./list-products.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush//Default
 })
-export class ListProductsComponent implements OnInit, OnDestroy {
-  products: IProduct[] = [];
+export class ListProductsComponent implements OnInit {
+  products?: Observable<IProduct[]>;
   productSubscription?: Subscription;
 
   constructor(private appService: ProductService,
     private cd: ChangeDetectorRef // OnPUsh doesn't detect changes from observable. use ChangeDetectorRef 
     ) { }
 
-  productObserver = {
+  /* productObserver = {
     next: (data: IProduct[]) => { 
       this.products = data; 
       this.cd.markForCheck(); //enable the component to detect changes from observable
     },
     error: (error: any) => { console.log(error) },
     complete: () => { console.log('product stream completed ') }
-  }
+  } */
 
   ngOnInit(): void {
-    this.productSubscription = this.appService.products$.subscribe(this.productObserver)
+    // this.productSubscription = this.appService.products$;//.subscribe(this.productObserver)
+    this.products = this.appService.products$;
   }
 
-  ngOnDestroy(): void {
-    if (this.productSubscription) {
-      this.productSubscription.unsubscribe();
-    }
-  }
+  // ngOnDestroy(): void {
+  //   if (this.productSubscription) {
+  //     this.productSubscription.unsubscribe();
+  //   }
+  // }
 }
